021d79d8045d93875b37c81621348b8ccb7d54c1
Louis Magdaleno <buildthatapp@gmail.com>
2019-05-28 16:25:50 -0700

    Updated Index.js to use routes

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const zooRouter = require('./routes/zooRoutes');
+
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.use('/api/zoos', zooRouter);
 
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
+
+module.exports = server;
